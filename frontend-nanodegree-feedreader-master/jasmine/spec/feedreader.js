55dd645060c31622f10c49ce9f312970c70c24f3
3otes <john@encoreservices.co.za>
2018-10-29 14:15:17 +0200

    Write a test that loops through each feed in the allFeeds object and ensures it has a URL defined and that the URL is not empty. TODO: Write a test that loops through each feed in the allFeeds object and ensures it has a name defined and that the name is not empty.

--- frontend-nanodegree-feedreader-master/jasmine/spec/feedreader.js
+++ frontend-nanodegree-feedreader-master/jasmine/spec/feedreader.js
@@ -31,12 +31,26 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+         it('URL is defined and not empty', function() {
+           allFeeds.forEach(function(feed) {
+             feedLink = feed.url;
+             expect(feedLink).toBeDefined();
+             expect(feedLink.length).not.toBe(0);
+           });
+         });
 
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+         it('name and not empty', function() {
+           allFeeds.forEach(function(feed) {
+             feedName = feed.name;
+             expect(feedName).toBeDefined();
+             expect(feedName.length).not.toBe(0);
+           });
+         });
     });
 
 
